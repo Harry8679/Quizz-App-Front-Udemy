e57cc31f26614c4da0ddc56e9db6a29e8a502cae
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-21 09:40:29 +0100

    Quiz Card

---
+++ src/components/QuizCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const QuizCard = ({ question, onAnswer }) => {
+  return (
+    <div className="border p-6 rounded-lg shadow-md">
+      <h2 className="text-xl font-bold mb-4">{question.question}</h2>
+      <div className="flex flex-col space-y-2">
+        {question.options.map((option, index) => (
+          <button
+            key={index}
+            className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+            onClick={() => onAnswer(option.isCorrect)}
+          >
+            {option.text}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuizCard;
