1f9823c7a2324474f0bdd9b7496106a516090a4f
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-23 20:36:28 +0100

    QuizCard 2

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -6,7 +6,7 @@ const QuizCard = ({ question, onAnswer }) => {
   return (
     <div className="border p-6 rounded-lg shadow-md">
       <h2 className="text-xl font-bold mb-4">
-        <BlockMath>{question.question}</BlockMath> 
+        <BlockMath>{question.question}</BlockMath>
       </h2>
       <div className="flex flex-col space-y-2">
         {question.options.map((option, index) => (
